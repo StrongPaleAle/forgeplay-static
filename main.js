561a2c7943dc82d6dfa149afbdf93d73b83e2acf
StrongAle <alessio.falchi90@gmail.com>
2023-04-26 19:05:17 +0100

    footer logo fix + basic modal

--- main.js
+++ main.js
@@ -1,2 +1,28 @@
-import './style.css'
+import './style.css';
 
+
+const dialogTriggers = document.querySelectorAll('[data-dialog]');
+
+dialogTriggers.forEach(triggerWrapper => {
+    const triggers = triggerWrapper.querySelectorAll('.dialog-open');
+    
+    const target = triggerWrapper.dataset.dialog;
+    console.log(triggers.length);
+    triggers.forEach(trigger => {
+        console.log(trigger);
+        trigger.addEventListener('click', () => {
+            console.log('click');
+            
+            const dialog = document.getElementById('game-1');
+            dialog.classList.add('open');
+
+            dialog.querySelectorAll('.dialog-close').forEach(close => {
+                close.addEventListener('click', () => {
+                    dialog.classList.remove('open');
+                });
+            });
+
+        });
+    });
+    
+});

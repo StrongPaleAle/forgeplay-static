1a5b07ec84181d0c907dbcef971805aa28ab9fef
StrongAle <alessio.falchi90@gmail.com>
2023-04-28 19:20:04 +0100

    dialog mobile and a11y, about image, form js

---
+++ form.js
@@ -0,0 +1,58 @@
+export function formHandle(formid){
+    const form = document.getElementById(formid);
+    console.log(form);
+    const acceptance = form.querySelector('input#form_acceptance');
+    const submit = form.querySelector('button[type="submit"]');
+
+    acceptance.addEventListener('change', function(){
+        console.log('acceptance');
+        if(this.checked){
+            submit.removeAttribute('disabled');
+        } else {
+            submit.setAttribute('disabled', 'disabled');
+        }
+    });
+
+    
+    submit.addEventListener('click', function(event){
+        event.preventDefault();
+        /*
+        * There is no shortcut like with 'new FormData(this)', we need
+        * to construct the form-object ourselves. We are creating a regular
+        * object instead of a FormData-object. `this` refers to the form,
+        * we could also write: form.username.value and form.favorite_number.value
+        */
+        const formattedFormData = {
+            name: form.form_name.value,
+            nickname: form.form_nickname.value,
+            email: form.form_email.value,
+            object: form.form_object.value,
+            message: form.form_message.value,
+            time: performance.now() 
+        }
+        //postData(formattedFormData);
+        console.log(formattedFormData);
+    });
+
+    // async function postData(formattedFormData){
+    //     /**
+    //      * The request is still 'POST' but the $_GET variable
+    //      * will get values too: 'name' and 'favorite_color'
+    //      */
+    //     const response = await fetch(
+    //         'handle_form.php',
+    //         {
+    //             method: 'POST',
+    //             /*
+    //             * We also need to stringify the values, turn the
+    //             * JavaScript object to a single string that is accepted
+    //             * as JSON. So we are sending one string that contains
+    //             * all our values
+    //             */
+    //             body: JSON.stringify(formattedFormData)
+    //         }
+    //     );
+    //     const data = await response.text();
+    //     console.log(data);
+    // }
+}
--- main.js
+++ main.js
@@ -1,25 +1,66 @@
 import '@splidejs/splide/css'
 import './style.css';
 import Splide from '@splidejs/splide';
+import { formHandle } from './form';
+
+
+
+formHandle('contact-form');
+
 
 const dialogTriggers = document.querySelectorAll('[data-dialog]');
 
 dialogTriggers.forEach(triggerWrapper => {
     const triggers = triggerWrapper.querySelectorAll('.dialog-open');
-    
+    const defaultTrigger = triggerWrapper.querySelector('button.dialog-open');
     const target = triggerWrapper.dataset.dialog;
-    console.log(triggers.length);
+    //console.log(triggers.length);
+
+
     triggers.forEach(trigger => {
-        console.log(trigger);
+        //console.log(trigger);
         trigger.addEventListener('click', () => {
-            console.log('click');
+            //console.log('click');
             
-            const dialog = document.getElementById('game-1');
+            const dialog = document.getElementById(target);
+            const dialogElements = 'input:not([disabled]), button:not([disabled])';
+            const focusableElements = dialog.querySelectorAll(dialogElements);
+
+            const firstFocusableElement = focusableElements[0];
+            const lastFocusableElement = focusableElements[focusableElements.length - 1];
+
+            
+
             dialog.classList.add('open');
+            dialog.setAttribute("tabindex", "-1");
+            dialog.focus();
+            
+            dialog.addEventListener('keydown', (event) => {
+
+                // handle TAB key
+                if (event.key === "Tab") {
+                    if (document.activeElement === lastFocusableElement) {
+                    event.preventDefault();
+                    firstFocusableElement.focus();
+                    }
+                }
+                // handle ESCAPE key to close the modal
+                if (event.key === "Escape" || event.key === "Esc") {
+                    dialog.classList.remove('open');
+                    dialog.removeAttribute("tabindex");
+                    // Focus to the register button (the last focus before modal was opened)
+                    defaultTrigger.focus();
+
+                }
+            });
+
+            mountDialogGallery(dialog);
 
             dialog.querySelectorAll('.dialog-close').forEach(close => {
                 close.addEventListener('click', () => {
                     dialog.classList.remove('open');
+                    dialog.removeAttribute("tabindex");
+                    defaultTrigger.focus();
                 });
             });
 
@@ -27,14 +68,26 @@ dialogTriggers.forEach(triggerWrapper => {
     });
     
 });
+function mountDialogGallery(parent){
+    var elms = parent.getElementsByClassName( 'splide' );
 
-var elms = document.getElementsByClassName( 'splide' );
-
-for ( var i = 0; i < elms.length; i++ ) {
-    console.log('activate' + elms[i]);
-  new Splide( elms[ i ], {
-    type     : 'loop',
-    focus    : 'center',
-    autoWidth: true,
-  }  ).mount();
+    for ( var i = 0; i < elms.length; i++ ) {
+        
+        if (elms[i].classList.contains('is-initialized')){
+            console.log('splide already active');
+            continue;
+        } else{
+            console.log('activation splide');
+            new Splide( elms[ i ], {
+                type     : 'loop',
+                focus    : 'center',
+                autoWidth: true,
+                gap: '1rem',
+                breakpoints: {
+                    640: { autoWidth: false, perPage: 1, gap: '0rem', type: 'slide' },
+                },
+            }  ).mount();
+        }
+        
+    }
 }

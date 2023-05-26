33d667933d900ce54591eafab4d2f74f2c277937
StrongAle <alessio.falchi90@gmail.com>
2023-05-26 18:44:23 +0100

    privacy and terms

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,13 @@ export default {
       partialDirectory: resolve(__dirname, 'partials'),
     }),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        "privacy-policy": resolve(__dirname, 'privacy-policy/index.html'),
+        "termini-e-condizioni": resolve(__dirname, 'termini-e-condizioni/index.html'),
+      }
+    }
+  }
 };

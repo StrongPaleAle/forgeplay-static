0baf0505ea5388839a228096704bd7fc189e9c43
StrongAle <alessio.falchi90@gmail.com>
2023-05-30 17:35:01 +0100

    handle_alphatest fix

--- public/handle_alphatest.php
+++ public/handle_alphatest.php
@@ -10,23 +10,21 @@ $data = json_decode(file_get_contents('php://input'), true);
     // sanitize the data and check for errors
     
     $email = $data['email'];
-    
-
-    
+       
 
     $sanitizedEmail = filter_var($email, FILTER_SANITIZE_EMAIL);
     if (!$sanitizedEmail || $sanitizedEmail == '' || !filter_var($sanitizedEmail, FILTER_VALIDATE_EMAIL)) {
-        $result['message'] = 'Please enter a valid email';
-    }
 
-    // if there are errors, return the errors
-    if (count($result['errors']) > 0) {
-        // return the errors
+        $result['message'] = 'Please enter a valid email';
         header($_SERVER['SERVER_PROTOCOL'] . ' 400 Bad Request');
         echo json_encode($result);
         exit;
+
     } else {
         // send the email
+        if (!file_exists("emails.txt")){
+            touch("emails.txt");
+        }
         $file = "emails.txt";
 		$current = file_get_contents($file);
 		$current .= $email . ",\n";
@@ -37,8 +35,5 @@ $data = json_decode(file_get_contents('php://input'), true);
         echo json_encode($result);
         exit;
     }
-        
-
-    
 
 ?>
